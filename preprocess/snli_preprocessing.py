dea0878e8121c9e23abb299318e28bea172d748e
js5991 <js5991@nyu.edu>
2017-11-18 23:03:46 -0500

    update preprocessing

--- preprocess/snli_preprocessing.py
+++ preprocess/snli_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import nltk
+import random
 
 def read_data_set(data_path):
     """
@@ -8,6 +9,7 @@ def read_data_set(data_path):
     return a dataset with 5 columns of tokens of sentence 1 and 2, sizes of tokens, and one hot vector as label
     data with gold_label of "-" are removed
     """
+    
     def tokenize_sentence(parse):
         """
         Read the given parse string
@@ -18,6 +20,17 @@ def read_data_set(data_path):
         token_list = tree.leaves()
         token_list.insert(0,"NULL")
         return token_list
+    
+    def tokenize_sentence_from_binary_parse(parse):
+        """
+        @param parse: a binary parse of the sentence
+        @return: token of the parsed sentence
+        """
+        token_list = ["<NULL>"]
+        if parse:
+            token_list  = token_list + parse.replace("(", "").replace(")", "").strip().split()
+        return token_list
+        
     
     def label_function (label):
         """
@@ -35,23 +48,70 @@ def read_data_set(data_path):
     data = pd.read_csv(data_path,delimiter="\t")
     data = data[data["gold_label"] != "-"]
     
-    data['sentence1_token'] = data.apply(lambda row: tokenize_sentence(row['sentence1_parse']), axis=1)
-    data['sentence2_token'] = data.apply(lambda row: tokenize_sentence(row['sentence2_parse']), axis=1)
+    data['sentence1_token'] = data.apply(lambda row: tokenize_sentence_from_binary_parse(row['sentence1_binary_parse']), axis=1)
+    data['sentence2_token'] = data.apply(lambda row: tokenize_sentence_from_binary_parse(row['sentence2_binary_parse']), axis=1)
     
     data['sentence1_size'] =  data.apply(lambda row: len(row['sentence1_token']), axis=1)
     data['sentence2_size'] = data.apply(lambda row: len(row['sentence2_token']), axis=1)
     data['max_size'] = data.apply(lambda row: max(row['sentence1_size'], row['sentence2_size']), axis=1)
+    data['min_size'] = data.apply(lambda row: min(row['sentence1_size'], row['sentence2_size']), axis=1)
+    
+    data = data[data["min_size"] > 1]
+    
     data['onehot_label'] = data.apply(lambda row: label_function(row['gold_label']), axis=1)
-    data = data.sort_values(by = 'max_size')
-    return data[['sentence1_token', 'sentence2_token', 'sentence1_size', 'sentence2_size', 'onehot_label']]
+    return data[['sentence1_token', 'sentence2_token', 'sentence1_size', 'sentence2_size', 'onehot_label', 'max_size']]
 
-def pad_sentence(token_list, pad_length):
-    """
-    PAD 0 in front of the token_list
-    return padded_list
-    """
 
-    padding = [0] * (pad_length - len(token_list))
-    padded_list = padding + token_list
-    return padded_list
 
+def batch_iter(dataframe, batch_size):
+    
+    def pad_sentence(token_list, pad_length):
+        """
+        PAD 0 in front of the token_list
+        @param token_list:
+        @param pad_length: 
+        @return: padded_list
+        """
+        padding = ["<PAD>"] * (pad_length - len(token_list))
+        padded_list = padding + token_list
+        return padded_list
+    
+    def shuffle_based_on_length (dataframe):
+        '''
+        It shuffles the index based on the length of the sentences
+        @param data_frame: a data frame
+        @return: list of shuffled index
+        '''
+        index_of_len_less_20 = dataframe.index[dataframe['max_size'] < 20].tolist()
+        index_other = dataframe.index[dataframe['max_size'] > 50].tolist()
+        index_of_len_less_50 = list(set(dataframe.index.tolist()) - set(index_of_len_less_20) - set(index_other))
+        
+
+        random.shuffle(index_of_len_less_20)
+        random.shuffle(index_of_len_less_50)
+        random.shuffle(index_other)
+
+        index = index_of_len_less_20 + index_of_len_less_50 + index_other
+        return index
+
+    start = - 1 * batch_size
+    index_list = shuffle_based_on_length (dataframe)
+    dataset_size = len(index_list)
+    
+    while True:
+        start += batch_size
+        
+        if start > dataset_size - batch_size:
+            # Start another epoch.
+            start = 0
+            index_list = shuffle_based_on_length (dataframe)
+        batch_indices = index_list[start:start + batch_size]
+        batch = dataframe.loc[batch_indices]
+        max_length = batch['max_size'].max()
+        batch['padded_sentence1'] = batch.apply(lambda row: pad_sentence(row['sentence1_token'], max_length), axis=1)
+        batch['padded_sentence2'] = batch.apply(lambda row: pad_sentence(row['sentence2_token'], max_length), axis=1)
+            
+        yield [batch['padded_sentence1'].tolist(), batch['padded_sentence2'].tolist(), batch['onehot_label'].tolist()]
+    
+   
+    
